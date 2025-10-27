aa492df4c24b16f0916d6bee7e9a43c158c2509f
Michael Alestock <m.alestock93@gmail.com>
2025-10-27 10:28:51 -0400

    [feat]: added import for utilities

--- DuoPy/__init__.py
+++ DuoPy/__init__.py
@@ -1,2 +1,7 @@
-from .secret_manager import SecretManager
-repr(SecretManager)
+from .secret_manager import secret_manager
+
+repr(secret_manager)
+
+from Utilities.utilities import utilities
+
+repr(utilities)
