4fdcc0d84f9a0ceb3cd9eebb5e41522c58c77aef
Michael Alestock <m.alestock93@gmail.com>
2025-10-27 10:56:40 -0400

    [fix]: propertly imported utilities

--- DuoPy/__init__.py
+++ DuoPy/__init__.py
@@ -2,6 +2,6 @@ from .secret_manager import secret_manager
 
 repr(secret_manager)
 
-from Utilities.utilities import utilities
+from .Utilities.utilities import utilities
 
 repr(utilities)
--- DuoPy/main.py
+++ DuoPy/main.py
@@ -1,1 +1,10 @@
-import secret_manager
+from secret_manager import secret_manager
+
+env_match = secret_manager.get_matched_file
+
+if not env_match:
+    secret_manager.write_secret_information()
+else:
+    print("It's working baby!")
+    secret_manager.print_match()
+    secret_manager.print_secret_information()
