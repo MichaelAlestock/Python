dcc45f0da18ede164927ca94490a3f6ba8e01d77
Anthony Alestock <m.alestock93@gmail.com>
2025-10-30 20:11:40 -0400

    [new]: moved convert_to_dt() into utilities module

--- DuoPy/Utilities/utilities.py
+++ DuoPy/Utilities/utilities.py
@@ -1,8 +1,15 @@
 # other imports
 from pathlib import Path
+from datetime import datetime
 
 
-class utilities:
+class Utilities:
 
     def script_root():
         return Path(__file__).resolve().parent
+
+    def convert_to_dt(date_item):
+        if type(date_item) is type(None):
+            return "No Date Info Found"
+        else:
+            return datetime.fromtimestamp(date_item).strftime("%m/%d/%Y @ %I:%M %p")
