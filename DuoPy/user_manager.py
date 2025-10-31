719bf7652faa2e979005ccf5755984507a5da205
Anthony Alestock <m.alestock93@gmail.com>
2025-10-31 07:09:23 -0400

    [feat]: added get_users_by_status() to find all users with a specific status

--- DuoPy/user_manager.py
+++ DuoPy/user_manager.py
@@ -135,24 +135,25 @@ class UserManager:
         except IndexError as ierror:
             ierror.add_note(f"No user found with the email address [{email_address}].")
 
-    def get_user_by_status(status):
+    def get_users_by_status(status):
         app = UserManager.client_admin()
-        response = app.get_users_by_name()
+        response = app.get_users()
 
         user_list = []
         for user in response:
-            user_list.append(
-                Users(
-                    realname=user["realname"],
-                    username=user["username"],
-                    email_address=user["email"],
-                    status=user["status"],
-                    created_on=Utilities.convert_to_dt(user["created"]),
-                    last_directory_sync=Utilities.convert_to_dt(
-                        user["last_directory_sync"]
-                    ),
-                    last_login=Utilities.convert_to_dt(user["last_login"]),
+            if user["status"] == status:
+                user_list.append(
+                    Users(
+                        realname=user["realname"],
+                        username=user["username"],
+                        email_address=user["email"],
+                        status=user["status"],
+                        created_on=Utilities.convert_to_dt(user["created"]),
+                        last_directory_sync=Utilities.convert_to_dt(
+                            user["last_directory_sync"]
+                        ),
+                        last_login=Utilities.convert_to_dt(user["last_login"]),
+                    )
                 )
-            )
         # TODO: Refactor to include error-handling in case this has more than 1 index
-        return user_list[0]
+        return user_list
