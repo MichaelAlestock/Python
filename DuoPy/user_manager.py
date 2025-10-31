8d2837f43cad5671258d5de51ea5f8019ee49950
Anthony Alestock <m.alestock93@gmail.com>
2025-10-30 20:11:06 -0400

    hard at work

--- DuoPy/user_manager.py
+++ DuoPy/user_manager.py
@@ -2,6 +2,7 @@ import duo_client
 from secret_manager import SecretManager
 from Users import Users
 from Utilities.utilities import Utilities
+import logging
 
 """
 # TODO: Flesh out UserManager class
@@ -14,6 +15,7 @@ class UserManager:
 
     # internal function to create and return a Duo Admin client object
     # used to by all other functions to make API calls
+    # TODO: Implement a unit test to test credentials work
     @staticmethod
     def client_admin():
         load_env = SecretManager.load_secret_information()
@@ -30,7 +32,6 @@ class UserManager:
             skey=skey,
             host=host,
             sig_timezone="UTC",
-            # TODO: Fix 401 error | caused by invalid credentials.. need them to test
         )
 
     def get_users():
@@ -58,19 +59,100 @@ class UserManager:
             generic_error.add_note("No results returned.")
             raise
 
-    def get_user_by_username():
-        return True
+    # TODO: Refactor to include error-handling for empty list
+    def get_user_by_username(username):
+        app = UserManager.client_admin()
+        response = app.get_users_by_name(username=username)
 
-    def get_user_by_phone_number():
-        return True
+        user_list = []
+        for user in response:
+            user_list.append(
+                Users(
+                    realname=user["realname"],
+                    username=user["username"],
+                    email_address=user["email"],
+                    status=user["status"],
+                    created_on=Utilities.convert_to_dt(user["created"]),
+                    last_directory_sync=Utilities.convert_to_dt(
+                        user["last_directory_sync"]
+                    ),
+                    last_login=Utilities.convert_to_dt(user["last_login"]),
+                )
+            )
+        # TODO: Refactor to include error-handling in case this has more than 1 index
+        return user_list[0]
 
-    def get_user_by_email_address():
-        return True
+    def get_user_by_phone_number(phone_number):
+        app = UserManager.client_admin()
+        response = app.get_users_by_name(username=phone_number)
 
-    def get_user_information(self):
-        UserManager.client_admin().get_users_by_name(self.username)
+        user_list = []
+        for user in response:
+            user_list.append(
+                Users(
+                    realname=user["realname"],
+                    username=user["username"],
+                    email_address=user["email"],
+                    status=user["status"],
+                    created_on=Utilities.convert_to_dt(user["created"]),
+                    last_directory_sync=Utilities.convert_to_dt(
+                        user["last_directory_sync"]
+                    ),
+                    last_login=Utilities.convert_to_dt(user["last_login"]),
+                )
+            )
+        # TODO: Refactor to include error-handling in case this has more than 1 index
+        return user_list[0]
 
-    def print_connected():
-        load_env = SecretManager.load_secret_information()
-        ikey = load_env.get("host")
-        print(ikey)
+    # TODO: Is there a better way to do this? Seems like it could take a while to return
+    def get_user_by_email_address(email_address):
+        app = UserManager.client_admin()
+        response = app.get_users()
+
+        user_list = []
+
+        # iterate over list and find the user who has the specified email
+        try:
+            for user in response:
+                if user["email"] == email_address:
+                    user_list.append(
+                        Users(
+                            realname=user["realname"],
+                            username=user["username"],
+                            email_address=user["email"],
+                            status=user["status"],
+                            created_on=Utilities.convert_to_dt(user["created"]),
+                            last_directory_sync=Utilities.convert_to_dt(
+                                user["last_directory_sync"]
+                            ),
+                            last_login=Utilities.convert_to_dt(user["last_login"]),
+                        )
+                    )
+                    return user_list[0]
+                else:
+                    return []
+            # TODO: Refactor to include error-handling in case this has more than 1 index
+        except IndexError as ierror:
+            ierror.add_note(f"No user found with the email address [{email_address}].")
+
+    def get_user_by_status(status):
+        app = UserManager.client_admin()
+        response = app.get_users_by_name()
+
+        user_list = []
+        for user in response:
+            user_list.append(
+                Users(
+                    realname=user["realname"],
+                    username=user["username"],
+                    email_address=user["email"],
+                    status=user["status"],
+                    created_on=Utilities.convert_to_dt(user["created"]),
+                    last_directory_sync=Utilities.convert_to_dt(
+                        user["last_directory_sync"]
+                    ),
+                    last_login=Utilities.convert_to_dt(user["last_login"]),
+                )
+            )
+        # TODO: Refactor to include error-handling in case this has more than 1 index
+        return user_list[0]
