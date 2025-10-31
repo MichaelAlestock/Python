549314b2701212ebefd2b013c638dea428db0365
Anthony Alestock <m.alestock93@gmail.com>
2025-10-30 20:12:07 -0400

    [fix]: updated class names

--- DuoPy/secret_manager.py
+++ DuoPy/secret_manager.py
@@ -5,10 +5,10 @@ from glob import glob
 from dotenv import load_dotenv
 
 # my libs
-from Utilities.utilities import utilities
+from Utilities.utilities import Utilities
 
 
-class secret_manager:
+class SecretManager:
     @staticmethod
     def get_next_argv(prompt):
         argv_iter = iter(sys.argv[1:])
@@ -19,19 +19,19 @@ class secret_manager:
 
     @staticmethod
     def get_matched_files():
-        wildcard = f"{utilities.script_root()}\\*.env"
+        wildcard = f"{Utilities.script_root()}\\*.env"
         return glob(wildcard)
 
     @staticmethod
     def get_matched_file():
-        matches = secret_manager.get_matched_files()
+        matches = SecretManager.get_matched_files()
         return matches[0] if matches else False
 
     def print_match():
-        print(secret_manager.get_matched_file())
+        print(SecretManager.get_matched_file())
 
     def load_secret_information():
-        load_dotenv(secret_manager.get_matched_file())
+        load_dotenv(SecretManager.get_matched_file())
 
         secret_dict = {
             "ikey": os.getenv("IKEY", ""),
@@ -42,7 +42,7 @@ class secret_manager:
         return secret_dict
 
     def print_secret_information():
-        load_env = secret_manager.load_secret_information()
+        load_env = SecretManager.load_secret_information()
         print(
             f"IKEY: [{load_env['ikey']}]\nSKEY: [{load_env['skey']}]\nHOST: [{load_env['host']}]"
         )
@@ -50,32 +50,32 @@ class secret_manager:
     def write_secret_information():
         # check file existence
         print("Checking if environment file exists...")
-        if not secret_manager.get_matched_file():
+        if not SecretManager.get_matched_file():
             print("No environment file found, create a new one.")
-            file_path = secret_manager.get_next_argv(
+            file_path = SecretManager.get_next_argv(
                 "Enter a name for your secrets file: "
             )
-            new_file_path = Path.joinpath(utilities.script_root(), file_path)
+            new_file_path = Path.joinpath(Utilities.script_root(), file_path)
             Path(f"{new_file_path}.env").touch()
-        elif secret_manager.GetMatchedFile():
-            print(f"{secret_manager.GetMatchedFile()} was found.")
+        elif SecretManager.GetMatchedFile():
+            print(f"{SecretManager.GetMatchedFile()} was found.")
 
             confirm_choice = input("Check secrets now? [y/N] ")
 
             if confirm_choice == "Y" or confirm_choice == "y":
                 print("Outputting your secrets.")
-                secret_manager.print_secret_information()
+                SecretManager.print_secret_information()
                 print("Exiting...")
                 exit(0)
             elif confirm_choice == "N" or confirm_choice == "n" or confirm_choice == "":
                 print("Exiting...")
                 exit(0)
 
-        ikey = secret_manager.get_next_argv("Enter your integration key: ")
-        skey = secret_manager.get_next_argv("Enter your secret key: ")
-        hostname = secret_manager.get_next_argv("Enter the hostname: ")
+        ikey = SecretManager.get_next_argv("Enter your integration key: ")
+        skey = SecretManager.get_next_argv("Enter your secret key: ")
+        hostname = SecretManager.get_next_argv("Enter the hostname: ")
 
-        print(f"Environment file successfully created in [{utilities.script_root()}].")
+        print(f"Environment file successfully created in [{Utilities.script_root()}].")
 
         with open(f"{new_file_path}.env", mode="w+t") as file:
             file.write(f"IKEY='{ikey}'\n" f"SKEY='{skey}'\n" f"HOST='{hostname}'")
