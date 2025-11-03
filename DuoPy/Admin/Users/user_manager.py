37157eada2637bae457c9feb94f524c276d12835
Anthony Alestock <alestockm@ccofnashua.org>
2025-11-03 07:49:46 -0500

    [refactor]: moved get_users_by_status to group with like functions

--- DuoPy/Admin/Users/user_manager.py
+++ DuoPy/Admin/Users/user_manager.py
@@ -66,6 +66,30 @@ class UserManager:
             generic_error.add_note("No results returned.")
             raise
 
+    def get_users_by_status(status):
+        app = UserManager.client_admin()
+        response = app.get_users()
+
+        user_list = []
+        for user in response:
+            # TODO: Error-handling for invalid status
+            if status in UserManager.ALLOWED_STATUSES and user["status"] == status:
+                user_list.append(
+                    Users(
+                        realname=user["realname"],
+                        username=user["username"],
+                        email_address=user["email"],
+                        status=user["status"],
+                        created_on=Utilities.convert_to_dt(user["created"]),
+                        last_directory_sync=Utilities.convert_to_dt(
+                            user["last_directory_sync"]
+                        ),
+                        last_login=Utilities.convert_to_dt(user["last_login"]),
+                    )
+                )
+        # TODO: Refactor to include error-handling in case this has more than 1 index
+        return user_list
+
     # TODO: Refactor to include error-handling for empty list
     def get_user_by_username(username):
         app = UserManager.client_admin()
