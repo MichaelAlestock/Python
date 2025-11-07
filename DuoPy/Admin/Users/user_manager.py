e4b7a060ff4eb542eadf8f95cc3b1f2350469e4e
Anthony Alestock <alestockm@ccofnashua.org>
2025-11-07 09:25:32 -0500

    [feat]: added sync_user function

--- DuoPy/Admin/Users/user_manager.py
+++ DuoPy/Admin/Users/user_manager.py
@@ -106,32 +106,23 @@ class UserManager:
         return user_list[0]
 
     # TODO: Is there a better way to do this? Seems like it could take a while to return
-    def get_user_by_email_address(email_address):
-        app = SecretManager.client_admin()
-        response = app.get_users()
-
-        user_list = []
-
-        # iterate over list and find the user who has the specified email
+    def sync_user(username: str) -> None:
+        # Grab dkey from secrets
         try:
-            for user in response:
-                if user["email"] == email_address:
-                    user_list.append(
-                        Users(
-                            realname=user["realname"],
-                            username=user["username"],
-                            email_address=user["email"],
-                            status=user["status"],
-                            created_on=Utilities.convert_to_dt(user["created"]),
-                            last_directory_sync=Utilities.convert_to_dt(
-                                user["last_directory_sync"]
-                            ),
-                            last_login=Utilities.convert_to_dt(user["last_login"]),
-                        )
-                    )
-                    return user_list[0]
-                else:
-                    return []
-            # TODO: Refactor to include error-handling in case this has more than 1 index
-        except IndexError as ierror:
-            ierror.add_note(f"No user found with the email address [{email_address}].")
+            LOAD_ENV = SecretManager.load_secret_information()
+
+            SecretManager.client_admin().sync_user(
+                username=username,
+                directory_key=LOAD_ENV.get("dkey"),
+            )
+
+            # Get user information so we can print that out to show the user was actually synced
+            user_information = UserManager.get_user_by_username(username)
+            user_last_directory_sync = user_information.last_directory_sync
+
+            print(
+                f"[{username}]successfully synced from external directory: {user_last_directory_sync}"
+            )
+        except RuntimeError:
+            print(f"User [{username}] not found in directory.")
+
